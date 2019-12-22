b01f5b4f9ec772c45f863cfc38112edaffe3cac1
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-22 17:56:54 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,5 +13,16 @@ class Triangle
     elsif @side_1 + @side_2 <= @side_3 || @side_1 + @side_3 <= @side_2 || @side_2 + @side_3 <= @side_1 
       raise TriangleError
     end
-  end 
+  if @side_1 == @side_2 && @side_1 == @side_3 
+      :equilateral
+    elsif @side_1 == @side_2 || @side_1 == @side_3 || @side_2 == @side_3
+      :isosceles 
+    else 
+      :scalene
+    end 
+  end
+  
+  class TriangleError < StandardError
+    
+  end
 end
